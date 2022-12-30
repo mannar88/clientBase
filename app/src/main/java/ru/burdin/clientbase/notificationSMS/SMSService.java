8267671fe3d28d25a6b7edccde885690f55ca5cd
Viktor <mannar@inbox.ru>
2022-12-30 15:25:32 +0500

    [18.4]  Убрана проблема связи с клиентом из расписания

--- app/src/main/java/ru/burdin/clientbase/notificationSMS/SMSService.java
+++ app/src/main/java/ru/burdin/clientbase/notificationSMS/SMSService.java
@@ -12,11 +12,13 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import ru.burdin.clientbase.Bd;
 import ru.burdin.clientbase.R;
 import ru.burdin.clientbase.models.Record;
 import ru.burdin.clientbase.notificationSMS.SendSMS;
+import ru.burdin.clientbase.setting.CalendarSetting;
 import ru.burdin.clientbase.setting.Preferences;
 import ru.burdin.clientbase.setting.TemplatesActivity;
 
@@ -27,8 +29,10 @@ import ru.burdin.clientbase.setting.TemplatesActivity;
         private List<Record> records = new ArrayList<>();
         private boolean checkBioolean = true;
         private Bd bd;
+private  DateFormat dateFormat = new SimpleDateFormat("dd-MM-YY");
+private  Context context;
 
-        public SMSService() {
+public SMSService() {
     }
 
     @Override
@@ -37,26 +41,27 @@ import ru.burdin.clientbase.setting.TemplatesActivity;
 
     }
 
-        /*
-        собирает список сеансов
-         */
-        private void setListSession(Context context) {
-            records.clear();
 
-            Calendar calendar = Calendar.getInstance();
-            if (check != TemplatesActivity.RADIO_BUTTON_TEMPLETES_MOTIFICATION_HOUR) {
-                if (dateFormat2.format(new Date()).equalsIgnoreCase(Preferences.getString(context.getApplicationContext(), Preferences.APP_PREFERENSES_TIME_NOTIFICATION_SMS, ""))) {
-                    DateFormat dateFormat = new SimpleDateFormat("dd-MM-YY");
-                    if (check == TemplatesActivity.RADIO_BUTTON_NOTIFICATION_NEXT_DAY) {
-                        calendar.add(Calendar.DAY_OF_MONTH, 1);
-                    }
-                    for (Record record : bd.getRecords()) {
-                        if (dateFormat.format(record.getStartDay()).equals(dateFormat.format(calendar.getTime()))) {
-                            records.add(record);
-                        }
+        /*
+        Собирает список клиентов следющего дня
+         */
+        private  void  getRecordNextDay(){
+            if (dateFormat2.format(new Date()).equals(Preferences.getString(getApplicationContext(), Preferences.APP_PREFERENSES_TIME_NOTIFICATION_SMS, ""))) {
+                Calendar calendar = Calendar.getInstance();
+                if (Preferences.getInt(context, Preferences.APP_PREFERENSES_CHECK_SMS_NOTIFICATION_1, TemplatesActivity.RADIO_DUTTON_TEMPLETES_NOTIFICATION_NOT_CHECK) == TemplatesActivity.RADIO_BUTTON_NOTIFICATION_NEXT_DAY) {
+                    calendar.add(Calendar.DAY_OF_MONTH, 1);
+                }
+                for (Record record : bd.getRecords()) {
+                    if (dateFormat.format(calendar.getTime()).equals(dateFormat.format(record.getStartDay()))) {
+                        records.add(record);
                     }
                 }
-            } else {
+            }
+            }
+/*
+Набор записей от конкретного времени
+ */
+            private  void  getNotificationHour() {
                 long time = Preferences.getLong(context, Preferences.APP_PREFERENCAES_TEMLETES_NOTIFICATION_HOUR, 0);
                 DateFormat dateFormat = new SimpleDateFormat("HH:mm dd-MM-YYYY");
                 for (Record record : bd.getRecords()) {
@@ -64,41 +69,49 @@ import ru.burdin.clientbase.setting.TemplatesActivity;
                         records.add(record);
                     }
                 }
-
             }
-        }
 
-
-        @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-//        registerReceiver(timeReceiver, new IntentFilter(                "android.intent.action.TIME_TICK"));
-        if (Preferences.getBoolean(getApplicationContext(), Preferences.APP_PREFERENSES_CHECK_AUTO_START_SERVICE, false)) {
-            Toast.makeText(this, "Сервис по отправки SMS запущен", Toast.LENGTH_SHORT).show();
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-
-                    while (check != TemplatesActivity.RADIO_DUTTON_TEMPLETES_NOTIFICATION_NOT_CHECK) {
-                        Context context = getApplicationContext();
-                        check = Preferences.getInt(getApplicationContext(), Preferences.APP_PREFERENSES_CHECK_SMS_NOTIFICATION_1, TemplatesActivity.RADIO_DUTTON_TEMPLETES_NOTIFICATION_NOT_CHECK);
-                        bd = Bd.load(getApplicationContext());
-                        setListSession(getApplication().getApplicationContext());
-                        if (records.size() > 0 && checkBioolean) {
-                            records.forEach(
-                                    record -> SendSMS.send(getApplicationContext(), Preferences.getString(context, SendSMS.KEY_PREFERENSES.get(1), SendSMS.TEMPLETS.get(1)), record, R.id.radioButtonAddSessionSMS)
-
+            /*
+Безконечный цикл сервиса
+ */
+            private  void  getStart() throws InterruptedException {
+                while (Preferences.getInt(getApplicationContext(), Preferences.APP_PREFERENSES_CHECK_SMS_NOTIFICATION_1, TemplatesActivity.RADIO_DUTTON_TEMPLETES_NOTIFICATION_NOT_CHECK) != 0) {
+                    records.clear();
+                    switch (Preferences.getInt(getApplicationContext(), Preferences.APP_PREFERENSES_CHECK_SMS_NOTIFICATION_1, TemplatesActivity.RADIO_DUTTON_TEMPLETES_NOTIFICATION_NOT_CHECK)) {
+                        case TemplatesActivity.RADIO_BUTTON_NOTIFICATION_NEXT_DAY:
+                            getRecordNextDay();
+                            break;
+                        case  TemplatesActivity.RADIO_BUTTON_NOTIFICATION_TODAY:
+                    getRecordNextDay();
+                            break;
+                        case TemplatesActivity.RADIO_BUTTON_TEMPLETES_MOTIFICATION_HOUR:
+                    getNotificationHour();
+                            break;
+                    }
+                    if (records.size() > 0) {
+                                                    records.forEach(
+                                    record -> SendSMS.send(context, Preferences.getString(context, SendSMS.KEY_PREFERENSES.get(1), SendSMS.TEMPLETS.get(1)), record, R.id.radioButtonAddSessionSMS)
                             );
-
-                            checkBioolean = false;
-                        }
-                        if (records.size() == 0) {
-                            checkBioolean = true;
-                        }
+                        Thread.sleep(1000 * 60);
                     }
                 }
-            };
-            Thread thread = new Thread(runnable);
-            thread.start();
+
+            }
+
+            @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+bd = Bd.load(getApplicationContext());
+        if (Preferences.getBoolean(getApplicationContext(), Preferences.APP_PREFERENSES_CHECK_AUTO_START_SERVICE, false)) {
+            Toast.makeText(this, "Сервис по отправки SMS запущен", Toast.LENGTH_SHORT).show();
+context = getApplicationContext();
+            Thread thread = new Thread(() -> {
+                try {
+                    getStart();
+                } catch (InterruptedException e) {
+                    Toast.makeText(this,e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+thread.start();
         }
         return Service.START_STICKY;
     }
